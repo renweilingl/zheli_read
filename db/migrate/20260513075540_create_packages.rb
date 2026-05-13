5f4bc4e615a37e3048111e72e1058eb1aa3a0ffd
renweilin <316955846@qq.com>
2026-05-13 16:03:58 +0800

    mere

--- db/migrate/20260513075540_create_packages.rb
+++ db/migrate/20260513075540_create_packages.rb
@@ -4,6 +4,7 @@ class CreatePackages < ActiveRecord::Migration[7.1]
       t.string :name,  comment: '套餐名字', limit: 16
       t.float :origin_price, comment: '原价'
       t.float :price, comment: '实际原价'
+      t.integer :sn, comment: '排序', :default => 0
       t.integer :effective_days, comment: '有效天数'
 
       t.timestamps
