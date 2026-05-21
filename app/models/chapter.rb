3343e33f5d7ab9680c412c17fa086a6682eea34d
renweilin <316955846@qq.com>
2026-05-21 14:45:04 +0800

    mere

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -9,28 +9,32 @@ class Chapter < ApplicationRecord
     epub: 'epub',
     pdf: 'pdf',
     mp3: 'mp3',
-    mp4: 'mp4'
+    mp4: 'mp4',
+    wav: 'wav',
   }.freeze
 
   FILE_TYPE_NAMES = {
     epub: 'EPUB电子书',
     pdf: 'PDF文档',
     mp3: '有声',
-    mp4: '视频'
+    mp4: '视频',
+    wav: '有声'
   }.freeze
 
   FILE_TYPE_ICONS = {
     epub: 'layui-icon-read',
     pdf: 'layui-icon-file-b',
     mp3: 'layui-icon-headset',
-    mp4: 'layui-icon-video'
+    mp4: 'layui-icon-video',
+    wav: 'layui-icon-headset'
   }.freeze
 
   FILE_TYPE_COLORS = {
     epub: '#10b981',
     pdf: '#ef4444',
     mp3: '#8b5cf6',
-    mp4: '#3b82f6'
+    mp4: '#3b82f6',
+    wav: '#8b5cf6'
   }.freeze
 
   # 验证
