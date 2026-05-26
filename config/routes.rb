5c49f901babd09fad9c5ecf7a2a1ace97d61391e
renweilin <316955846@qq.com>
2026-05-26 15:37:25 +0800

    mere

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Root route
-  root "pages#home"
+  root "admin/grades#index"
 
   # Login routes
   get "login", to: "sessions#new"
