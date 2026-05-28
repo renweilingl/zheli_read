1ad10922fa13d59d5cd5bc5e23d6c57e66443bd5
renweilin <316955846@qq.com>
2026-05-28 14:42:42 +0800

    mere

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,18 +1,67 @@
 class RanksController < ApplicationController
   before_action :require_login
   before_action :set_grade
+  before_action :set_rank, only: [:edit, :update, :destroy]
 
   def index
+    authorize Rank
   end
 
   def new
+    rank = @grade.ranks.order("sn desc").first
+    sn = rank.nil? ? 1 : rank.sn + 1
+
+    @rank = @grade.ranks.new(sn: sn, category_id: 1)
+    authorize @rank
   end
 
   def create
+    @rank = @grade.ranks.new(rank_params)
+    authorize @rank
+
+    if @rank.save!
+      flash[:success] = '排行榜创建成功'
+      redirect_to grade_ranks_path(@grade)
+    else
+      flash[:error] = @rank.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def edit
+    authorize @rank
+  end
+
+  def update
+    authorize @rank
+
+    if @rank.update(rank_params)
+      flash[:success] = '排行榜等级更新成功'
+      redirect_to grade_ranks_path(@grade)
+    else
+      flash[:error] = @rank.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @rank
+    @rank.destroy
+
+    flash[:success] = '排行榜删除成功'
+    redirect_to grade_ranks_path(@grade)
   end
 
   private
   def set_grade
     @grade = Grade.find(params[:grade_id])
+  end
+
+  def set_rank
+    @rank = Rank.find(params[:id])
+  end
+
+  def rank_params
+    params.require(:rank).permit(:name, :sn, :category_id)
   end
 end
---
+++ app/policies/rank_policy.rb
@@ -0,0 +1,35 @@
+class RankPolicy < ApplicationPolicy
+  def index?
+    user.super_admin? || user.editor?
+  end
+  
+  def show?
+    user.super_admin? || user.editor?
+  end
+  
+  def new?
+    create?
+  end
+  
+  def create?
+    user.super_admin? || user.editor?
+  end
+  
+  def edit?
+    update?
+  end
+  
+  def update?
+    user.super_admin? || user.editor?
+  end
+  
+  def destroy?
+    user.super_admin?
+  end
+  
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
