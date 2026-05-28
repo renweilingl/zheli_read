c6b71d42a217f33e2cd7006bd162dba35e3bf97f
renweilin <316955846@qq.com>
2026-05-28 16:38:56 +0800

    mere

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -4,6 +4,14 @@ class Rank < ApplicationRecord
   belongs_to :category
   has_many :rank_contents, dependent: :destroy
 
+  def content_names
+    res = []
+    rank_contents.each do |x|
+      res << x.content_name
+    end
+    res.join(" | ")
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["category_id", "created_at", "grade_id", "id", "id_value", "name", "sn", "updated_at"]
   end
