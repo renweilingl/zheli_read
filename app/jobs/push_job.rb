f04e7cd4a3b9955ca5c8a7addd9e3d507c464698
renweilin <316955846@qq.com>
2026-06-10 14:36:22 +0800

    mere

---
+++ app/jobs/push_job.rb
@@ -0,0 +1,33 @@
+class PushJob < ApplicationJob
+  queue_as :zheli_high
+  sidekiq_options retry: false
+
+  def perform(push_notification_id)
+    if Rails.env.production?
+      url = "https://api.zheliyuedu.cn/api/push/send"
+
+      pn = PushNotification.find push_notification_id
+
+      logger.info " PushJob: #{push_notification_id}"
+      return if pn.nil?
+
+      return if pn.push_scope  == 2  #先不针对特殊用户推送
+
+      type = pn.push_type == 0 ? "system" : "activity"
+
+      grade_ids = pn.push_scope == 0  ?  pn.grade_ids : Grade.all.pluck(:id)
+
+      data = {uuid: AppUser.first.uuid,
+              grade_ids: grade_ids,
+              title: pn.title,
+              body: pn.body,
+              type: pn.type,
+              content: {url: pn.link_url}
+      }
+
+      res = HTTParty.post(url, body: data.to_json, headers: {'Content-Type' => 'application/json'}).body
+      logger.info "PushJob push_notification_id: #{push_notification_id}, res: #{res}"
+    end
+  end
+
+end
