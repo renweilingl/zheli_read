247542b99620fdcbf9d6da9e1359ecd8c1a6c5ac
renweilin <316955846@qq.com>
2026-06-11 09:26:05 +0800

    emre

--- app/controllers/splash_ads_controller.rb
+++ app/controllers/splash_ads_controller.rb
@@ -68,10 +68,7 @@ class SplashAdsController < ApplicationController
     authorize @splash_ad
     @splash_ad.destroy
     
-    respond_to do |format|
-      format.html { redirect_to splash_ads_path, notice: '开屏广告已删除。' }
-      format.json { render json: { success: true } }
-    end
+    redirect_to splash_ads_path, notice: '开屏广告已删除。'
   end
   
   def publish
