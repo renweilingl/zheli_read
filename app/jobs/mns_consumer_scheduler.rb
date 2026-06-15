f23ea7b04861827d61b77c60cfb4970320e2868e
renweilin <316955846@qq.com>
2026-06-15 10:26:06 +0800

    emre

--- app/jobs/mns_consumer_scheduler.rb
+++ app/jobs/mns_consumer_scheduler.rb
@@ -12,9 +12,11 @@ class MnsConsumerScheduler
     MnsConsumerWorker.new.perform
     
     # 延迟指定时间后再次执行,形成持续监听
-    self.class.perform_in(CHECK_INTERVAL.seconds)
+    if Rails.env.production?
+      self.class.perform_in(CHECK_INTERVAL.seconds)
+    end
     
-    logger.info "MNS消费者调度完成,#{CHECK_INTERVAL}秒后再次检查"
+    #logger.info "MNS消费者调度完成,#{CHECK_INTERVAL}秒后再次检查"
   rescue => e
     logger.error "MNS消费者调度失败: #{e.message}"
     # 即使出错也继续调度,保证服务不中断
