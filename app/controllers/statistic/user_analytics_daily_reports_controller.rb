f7a9949b649aa9fb9926f27b0fb071444db4c433
renweilin <316955846@qq.com>
2026-06-30 14:41:14 +0800

    mere

--- app/controllers/statistic/user_analytics_daily_reports_controller.rb
+++ app/controllers/statistic/user_analytics_daily_reports_controller.rb
@@ -7,5 +7,21 @@ class Statistic::UserAnalyticsDailyReportsController < ApplicationController
 
   private
   def get_data
+    cond = {}
+
+    @start_date = Time.now.to_date - 7.days
+    @end_date = Time.now.to_date
+    if params[:start_date].present?
+      @start_date = params[:start_date]
+      cond[:stat_date_gteq] = Date.parse(params[:start_date]).strftime("%Y-%m-%d")
+    end
+
+    if params[:end_date].present?
+      @end_date = params[:end_date]
+      cond[:stat_date_lteq] = Date.parse(params[:end_date]).strftime("%Y-%m-%d")
+    end
+
+    @q = UserAnalyticsDaily.ransack(cond)
+    @resource = @q.result
   end
 end
