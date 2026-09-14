84161dd5092d85abee464b27510c2e3a6a0e57d8
renweilin <316955846@qq.com>
2026-09-14 10:44:44 +0800

    mere

--- db/migrate/20260910070000_change_plain_text_to_mediumtext.rb
+++ db/migrate/20260910070000_change_plain_text_to_mediumtext.rb
@@ -1,9 +1,9 @@
 class ChangePlainTextToMediumtext < ActiveRecord::Migration[7.1]
-  def up
-    change_column :ebook_pages, :plain_text, :mediumtext
-  end
+  #def up
+  #  change_column :ebook_pages, :plain_text, :mediumtext
+  #end
 
-  def down
-    change_column :ebook_pages, :plain_text, :text
-  end
+  #def down
+  #  change_column :ebook_pages, :plain_text, :text
+  #end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_02_112112) do
+ActiveRecord::Schema[7.1].define(version: 2026_09_10_070000) do
   create_table "app_users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "nickname"
     t.string "avatar"
@@ -388,7 +388,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_02_112112) do
   end
 
   create_table "orders", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", comment: "订单表", force: :cascade do |t|
-    t.integer "user_id", null: false, comment: "用户ID"
+    t.integer "app_user_id", null: false, comment: "用户ID"
     t.string "order_no", null: false, comment: "订单号"
     t.decimal "amount", precision: 10, scale: 2, null: false, comment: "金额"
     t.integer "payment_method", default: 0, null: false, comment: "支付方式：0-支付宝 1-微信"
@@ -398,10 +398,10 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_02_112112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "channel", limit: 32, comment: "支付渠道（继承自用户）"
+    t.index ["app_user_id"], name: "index_orders_on_user_id"
     t.index ["membership_id"], name: "index_orders_on_membership_id"
     t.index ["order_no"], name: "index_orders_on_order_no", unique: true
     t.index ["status"], name: "index_orders_on_status"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "packages", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
